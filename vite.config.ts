1e0b8d41ba5fd6bc9a7f5ccf8cc308bd211664cb
Lim Boseul <limbs722@gmail.com>
2024-07-29 12:07:49 +0900

    feat: Header 추가
    - UI frame 만 추가한 상태
    - emotion css 적용 위해 관련 라이브러리 설치, config 파일 수정

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,20 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { TanStackRouterVite } from '@tanstack/router-vite-plugin';
 import svgrPlugin from 'vite-plugin-svgr';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         react({
             jsxImportSource: '@emotion/react',
+            babel: {
+                plugins: ['@emotion/babel-plugin'],
+            },
         }),
         svgrPlugin(),
         TanStackRouterVite(),
+        tsconfigPaths(),
     ],
     server: {
         port: 3000,
