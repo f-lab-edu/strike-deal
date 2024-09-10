5169d3f919966fe975198ebeec281e46bb714c83
Lim Boseul <limbs722@gmail.com>
2024-09-10 13:03:47 +0900

    fix: getBeautifiedNum 함수 수정
    - 기존의 정규표현식 대신 세자리 마다 ',' 를 추가하도록 변경

--- src/utils.ts
+++ src/utils.ts
@@ -1,2 +1,14 @@
-export const getBeautifiedNum = (val: number) =>
-    val < 1000 ? val : val.toString().replace(/\d(?=(\d{3})+$)/g, '$&,');
+export const getBeautifiedNum = (val: number): string => {
+    const str = val.toString().split('').reverse();
+
+    // reduce를 사용하여 3자리마다 쉼표를 추가
+    const formatted = str.reduce((acc, digit, index) => {
+        // 인덱스가 3의 배수일 때마다(첫 번째 자리 제외) 쉼표를 추가
+        if (index % 3 === 0 && index !== 0) {
+            return digit + ',' + acc;
+        }
+        return digit + acc;
+    }, '');
+
+    return formatted;
+};
