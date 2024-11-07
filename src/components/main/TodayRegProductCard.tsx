299b92b6b998bd72dce65e2165b3d1bf284fb56a
Lim Boseul <limbs722@gmail.com>
2024-11-07 15:13:16 +0900

    fix: 컴포넌트 memo로 감싸기
    - 불필요한 렌더링 방지
    - 이미지 링크 변경

--- src/components/main/TodayRegProductCard.tsx
+++ src/components/main/TodayRegProductCard.tsx
@@ -5,13 +5,14 @@ import type { TodayRegProductType } from './types';
 
 const { Meta } = Card;
 
-const TodayRegProductCard = ({ product }: { product: TodayRegProductType }) => {
+const TodayRegProductCard = React.memo(({ product }: { product: TodayRegProductType }) => {
     const { title, img, description } = product;
+
     return (
         <Card hoverable css={mainStyle.productCard} cover={<img alt="product_img" src={img} />}>
             <Meta title={title} description={description} />
         </Card>
     );
-};
+});
 
 export default TodayRegProductCard;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -15,7 +15,7 @@ const products: TodayRegProductType[] = [
         createdDate: '2024-08-31',
         title: '윌슨 투수 글러브 팝니다.',
         description: '회전근개 파열로 투수 관둡니다.',
-        img: 'https://www.yagooshop.com/shop/data/goods/1724833446916m0.jpg',
+        img: 'https://godomall-storage.cdn-nhncommerce.com/ac23de62364d8a0eb93706f0d0f27a00/goods/1000005220/image/magnify/1000005220_magnify_046.png',
         price: '200000',
         seller: {
             id: 'user_00001',
@@ -34,7 +34,7 @@ const products: TodayRegProductType[] = [
         createdDate: '2024-08-31',
         title: '윌슨 투수 글러브 팝니다.',
         description: '회전근개 파열로 투수 관둡니다.',
-        img: 'https://www.yagooshop.com/shop/data/goods/1724833446916m0.jpg',
+        img: 'https://godomall-storage.cdn-nhncommerce.com/ac23de62364d8a0eb93706f0d0f27a00/goods/1000005220/image/magnify/1000005220_magnify_046.png',
         price: '200000',
         seller: {
             id: 'user_00001',
@@ -53,7 +53,7 @@ const products: TodayRegProductType[] = [
         createdDate: '2024-08-31',
         title: '윌슨 투수 글러브 팝니다.',
         description: '회전근개 파열로 투수 관둡니다.',
-        img: 'https://www.yagooshop.com/shop/data/goods/1724833446916m0.jpg',
+        img: 'https://godomall-storage.cdn-nhncommerce.com/ac23de62364d8a0eb93706f0d0f27a00/goods/1000005220/image/magnify/1000005220_magnify_046.png',
         price: '200000',
         seller: {
             id: 'user_00001',
